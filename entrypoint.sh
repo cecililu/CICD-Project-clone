5f545abb8a42bd9386b65af5114991984ce4146f
Cecil Ghimire <cecilghimire@gmail.com>
2023-07-30 16:06:25 +0545

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 python3 manage.py collectstatic --noinput
 #python manage.py makemigrations --merge --noinput
 python3 manage.py migrate --noinput
-python manage.py qcluster &
+# python manage.py qcluster &
 uwsgi --ini uwsgi.ini
